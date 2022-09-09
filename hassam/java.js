a8ea4971bfb4c40210f26fc1732305b12c68e4d6
Hassam197 <112552470+Hassam197@users.noreply.github.com>
2022-09-09 15:22:48 +0500

    Add files via upload

---
+++ hassam/java.js
@@ -0,0 +1,82 @@
+////document.write("Hassam" + "<br>");
+//var a1 = 20;
+//var b1 = 43;
+//document.write(a1 - b1,"<br>");
+
+//let a = 123;
+////let b = 21;
+//document.write(a / b );
+
+//const car = ("audi", "toyota","honda")
+
+
+//var a1 = "hassam";
+//var b1 = "din";
+//document.write("My name is:", "" + a1 + " " + b1+ "<br>" );
+
+//var a1 = "ayaz";
+//var b1 = "ali";
+//document.write("My father name is:", "" + a1 + " " + b1);
+
+//var a1 = 20
+//var b1 = 6;
+//document.write("My name is:", "" + a1 + " " + b1+ "<br>" );
+
+//var a1 = "ayaz";
+//var b1 = "ali";
+//document.write("My father name is:" + "" + a1 + " " + b1);
+
+
+//x = parseInt(prompt("enter a number:"));
+// y = parseInt(prompt("enter a number:"));
+// result = x + y;
+// console.log(result);
+// document.write(result)
+
+// x = parseInt(prompt("enter a number"))
+// y = parseInt(prompt("enter a number"))
+// result = x + y
+// console.log(result)
+// document.write(result)
+
+// x = parseInt(prompt("enter a number"))
+// y = parseInt(prompt("enter a number"))
+// result = x - y
+// console.log(result)
+// document.write(result)
+// 
+// x = parseInt(prompt("enter a number"))
+// y = parseInt(prompt("enter a number"))
+// result = x / y
+// console.log(result)
+// document.write(result)
+
+// x = parseInt(prompt("enter a number"))
+// y = parseInt(prompt("enter a number"))
+// result = x * y
+// console.log(result)
+// document.write(result)
+
+// window.alert("my name is Hassam");
+// document.write("<br>")
+
+// let z = 2;
+// let f = 5;
+// document.write(z ** f);
+// document.write("<br>")
+
+//  const x = "hassam"
+//  document.write(typeof (x));
+//  document.write("<br>")
+
+
+// document.write(Date());
+// document.write("<br>")
+
+// const t = 5;
+// document.write(Math.pow(t, 2));
+
+// for (let i = 0; i < 19; i--) {
+    // document.write(i, "<br>")
+// }
+
